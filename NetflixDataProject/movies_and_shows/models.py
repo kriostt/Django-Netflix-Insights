4941d84588cacd828cf8234819cbe1ead6648d00
devnexuss <129311516+devnexuss@users.noreply.github.com>
2024-12-04 20:15:36 -0700

    Define NetflixShow model

--- NetflixDataProject/movies_and_shows/models.py
+++ NetflixDataProject/movies_and_shows/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class NetflixShow(models.Model):
+    show_id = models.CharField(max_length=50, unique=True)  # Unique ID for each show
+    type = models.CharField(max_length=50)  # Movie or TV Show
+    title = models.CharField(max_length=200)  # Show title
+    director = models.CharField(max_length=200, null=True, blank=True)  # Director(s), can be empty
+    cast = models.CharField(max_length=500, null=True, blank=True)  # Cast, can be empty
+    country = models.CharField(max_length=200, null=True, blank=True)  # Country, can be empty
+    date_added = models.DateField(null=True, blank=True)  # Date when the show was added to Netflix
+    release_year = models.IntegerField()  # Year of release
+    rating = models.CharField(max_length=50)  # Rating (e.g., PG, R)
+    duration = models.CharField(max_length=50, null=True, blank=True)  # Duration (only for movies)
+    listed_in = models.CharField(max_length=200)  # Genre or categories
+    description = models.TextField(null=True, blank=True)  # Description of the show
+
+    def __str__(self):
+        return self.title
