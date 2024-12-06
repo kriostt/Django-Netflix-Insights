552530e04dc68d60ef7755a75ebd494d46cc8f7e
kriostt <129346100+kriostt@users.noreply.github.com>
2024-12-06 01:17:35 -0700

    Add comments

--- NetflixDataProject/movies_and_shows/urls.py
+++ NetflixDataProject/movies_and_shows/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Defines URL patterns for this app
 urlpatterns = [
+    # Maps root to analysis_view function in views
     path('', views.analysis_view)
 ]
